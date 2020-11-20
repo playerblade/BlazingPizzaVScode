7927629f77e271a945eeecf942d595d248480972
playerblade <evotar156@gmail.com>
2020-11-20 14:35:32 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazingPizzaVScode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
